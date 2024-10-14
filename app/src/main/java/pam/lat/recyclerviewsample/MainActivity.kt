18aa202a690e8e5cd36badea8d49c77a9d192488
nfzmhn <zidninurfauzi2203@gmail.com>
2024-10-14 17:53:08 +0700

    Fix Banget

--- app/src/main/java/pam/lat/recyclerviewsample/MainActivity.kt
+++ app/src/main/java/pam/lat/recyclerviewsample/MainActivity.kt
@@ -14,34 +14,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        // getting the recyclerview by its id
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
 
-
-        // this creates a vertical layout Manager
         recyclerview.layoutManager = LinearLayoutManager(this)
 
-
-        // ArrayList of class ItemsViewModel
         val data = ArrayList<ItemsViewModel>()
 
-
-        // This loop will create 20 Views containing
-        // the image with the count of view
         for (i in 1..20) {
-            data.add(ItemsViewModel("Item " + i))
-
-
+            data.add(ItemsViewModel("Item $i"))
         }
 
-
-        // This will pass the ArrayList to our Adapter
         val adapter = CustomAdapter(data)
         Log.d("test string","Jumlah data: "+data.size)
 
-
-        // Setting the Adapter with the recyclerview
         recyclerview.adapter = adapter
     }
 }
